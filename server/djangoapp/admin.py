dc64a8cfc506d0140107ff827472f76e4d88204d
Madhvika111 <80045321+Madhvika111@users.noreply.github.com>
2024-06-05 15:02:32 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,4 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import AboutUs
 
-
-# Register your models here.
-
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(AboutUs)
